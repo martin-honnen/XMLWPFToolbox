b603feb8fa4cda18e02a9399ff40cde98194d4c6
Martin Honnen <martin.honnen@gmx.de>
2026-05-08 13:23:58 +0200

    Update to latest PhoenixmlDb.XQuery and XSLT packages (XSLT can now transpile Schematron with Schxslt2 and validate against the transpilation result)

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -468,7 +468,7 @@ declare option output:indent ""yes"";
 
         private void AboutXMLToolbox_Executed(object sender, ExecutedRoutedEventArgs e)
         {
-            MessageBox.Show("XSLT 3.0, XQuery 3.1, XPath 3.1, XSD 1.1 XML Toolbox using Saxon " + processor.getSaxonEdition() + " " + processor.getSaxonProductVersion() + " or PhoenixmlDb.Xslt 1.2.5/PhoenixmlDb.XQuery 1.2.2" +  $" and {org.apache.xerces.impl.Version.getVersion()} run under {Environment.OSVersion} .NET {Environment.Version}", "About XSLT 3.0/XQuery 3.1/XPath 3.1/XSD 1.1 Toolbox");
+            MessageBox.Show("XSLT 3.0, XQuery 3.1, XPath 3.1, XSD 1.1 XML Toolbox using Saxon " + processor.getSaxonEdition() + " " + processor.getSaxonProductVersion() + " or PhoenixmlDb.Xslt 1.3.2/PhoenixmlDb.XQuery 1.3.0" +  $" and {org.apache.xerces.impl.Version.getVersion()} run under {Environment.OSVersion} .NET {Environment.Version}", "About XSLT 3.0/XQuery 3.1/XPath 3.1/XSD 1.1 Toolbox");
         }
 
         private string LoadFileIntoEditor(ICSharpCode.AvalonEdit.TextEditor editor, string filter, RadioButton type)
